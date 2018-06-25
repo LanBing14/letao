49b436d6ac193c19fa52591ee4951b984af25de1
lanbing <1413508146@qq.com>
2018-06-25 15:54:39 +0800

    登录进度条及状态修改完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,5 +1,13 @@
+// 进行表单配件验证
+
+
 $(function () {
     $('#form').bootstrapValidator({
+        feedbackIcons:{
+            valid:'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
         fields: {
             username:{
                 validators:{
@@ -10,6 +18,9 @@ $(function () {
                         min: 2,
                         max: 6,
                         message: "用户名长度必须在 2-6位"
+                    },
+                     callback: {
+                        message: "用户名不存在"
                     }
                 }
             },
@@ -22,9 +33,43 @@ $(function () {
                         min: 6,
                         max: 12,
                         message: "密码长度必须在 6-12位"
+                    },
+                    callback: {
+                        message: "密码错误"
                     }
                 }
             }
         }
+    });
+
+    // 需要注册表单成功事件
+    $('#form').on('success.form.bv', function (e) {
+        //阻止默认事件
+        e.preventDefault();
+        //ajax 提交
+        $.ajax({
+            type:'post',
+            url:'/employee/employeeLogin',
+            data:$('#form').serialize(),
+            dataType: 'json',
+            success: function (info) {
+
+                if (info.success) {
+                    location.href = "index.html";
+                };
+
+                if (info.error === 1000) {
+                    $('#form').data("bootstrapValidator").updateStatus("username","INVALID","callback")
+
+                };
+                if (info.error === 1001) {
+                    $('#form').data("bootstrapValidator").updateStatus("password","INVALID","callback")
+                }
+            }
+        })
+    });
+    $("#reset").on('click',function () {
+        $("#form").data("bootstrapValidator").resetForm();
     })
+
 })
