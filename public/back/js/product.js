fd690ba659480373518b43ae317ad01f9bda42b7
lanbing <1413508146@qq.com>
2018-06-27 12:03:01 +0800

    product完成基本页面

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -13,7 +13,7 @@ $(function () {
             },
             dataType: "json",
             success: function (info) {
-                console.log(info);
+                // console.log(info);
 
                 //模板
                 $('tbody').html(template('tmp',info));
@@ -39,28 +39,144 @@ $(function () {
     }
     render();
 
-    //
+    //点击添加按钮,模态框显示,向服务器发送请求
     $('#btnAdd').click(function () {
-        $('#myProductModal').modal('show')
+        $('#myProductModal').modal('show');
+        $.ajax({
+            type: 'get',
+            url:'/category/querySecondCategoryPaging',
+            data: {
+                page: 1,
+                pageSize: 100,
+            },
+            dataType:'json',
+            success: function (info) {
+                // console.log(info);
+                //准备模板
+                $('.dropdown-menu').html(template('tpl',info))
+            }
+        })
     });
 
+    //事件委托
+    $('.dropdown-menu').on('click','a',function () {
+        var text = $(this).text();
+        $('#categorySecond').text(text);
 
+        $id = $(this).data('id');
 
+        $('[name="brandId"]').val($id);
+    })
+    var picArr = [];
+   $('#fileupload').fileupload({
+       dataType: "json",
+       done: function (e,data) {
+        console.log(data);
+        $('#imgBox').show();
+        // console.log(data.result.picAddr);
 
-    $("#fileupload").fileupload({
-        dataType: 'json',
-        //图片上传后的对象,通过e.result.picAddr可以获取图片地址
-        done: function (e,data) {
-          console.log(data.result.picAddr);
-          var picUrl = data.result.picAddr;
-  
-          //将图片地址给img
-          //$('#imgBox img').attr('src',picUrl);
-  
-          //将图片的地址保存在brandLogo的隐藏雨中
-          //$('[name="brandLogo"]').val(picUrl);
-          // 手动将表单校验状态重置成 VALID
-        //$('#secondForm').data("bootstrapValidator").updateStatus("brandLogo", "VALID");
+        //获取的片的地址
+        var picUrl = data.result.picAddr;
+        //将图片的地址放到数组zhong
+        picArr.unshift(picUrl);
+
+        //新的图片添加到盒子的最前面
+        $('#imgBox').prepend('<img src="'+ picUrl +'"width="100" height="100">');
+        //上传头像大于三个就要将最后旧的一个删除
+        if(picArr.length > 3) {
+            picArr.pop();
+            //将最后一个创建的标签也要删除
+            $('#imgBox img:last-of-type').remove()
+        }
+       if (picArr.length === 3) {
+           $('#form').data('bootstrapValidator').updateStatus('picStatus','VALID')
+       } 
+       }
+   })
+   //校验表格
+    $('#form').bootstrapValidator({
+            excluded: [],
+            //配置图标
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+        },
+            fields: {
+                brandId: {
+                    validators:{
+                        notEmpty: {
+                            message: "请选择二级分类"
+                        }
+                    }    
+                },
+                proName: {
+                    validators:{
+                        notEmpty: {
+                            message: "请选择商品名称",
+                        }
+                    } 
+                },
+                proDesc: {
+                    validators:{
+                        notEmpty: {
+                            message: "请描述商品信息",
+                        }
+                    } 
+                },
+                num: {
+                    validators:{
+                        notEmpty: {
+                            message: "请输入商品数量",
+                        }
+                    } 
+                },
+                size: {
+                    validators:{
+                        notEmpty: {
+                            message: "请输入商品尺寸",
+                        }
+                    } 
+                },
+                oldPrice: {
+                    validators:{
+                        notEmpty: {
+                            message:  "请输入商品原价",
+                        }
+                    } 
+                },
+                price: {
+                    validators:{
+                        notEmpty: {
+                            message: "请输入商品现价",
+                        }
+                    } 
+                },
+                picStatus: {
+                    validators:{
+                        notEmpty: {
+                            message: "请选择三张商品的图片",
+                        }
+                    } 
+                }
         }
     })
+
+    //表单校验成功阻止默认事件
+    $('#form').on('success.form.bv', function (e) {
+        e.preventDefault();
+        //发送ajax请求
+        $.ajax({
+            type: 'post',
+            url: '/product/addProduct',
+            data: $('#form').serialize(),
+            dataType: 'json',
+            success: function (info) {
+                console.log(info);
+                currentPage = 1;
+                render();
+            }
+        })
+    })
+        
 })
